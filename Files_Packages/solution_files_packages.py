1ce38b337a49f0a78c469b18ea2b66be5d90f60f
LorinUNC <40806625+LorinUNC@users.noreply.github.com>
2019-09-30 16:56:38 -0400

    Update solution_files_packages.py

--- Files_Packages/solution_files_packages.py
+++ Files_Packages/solution_files_packages.py
@@ -11,5 +11,5 @@ for fname in flist:
         
     for s in seasons:
         for ingredient in seasons[s]:           
-             if (ingredient in recipe.lower()):  #.lower() changes all text in the recipe to lowercase
-                shutil.copyfile(fname, os.path.join(s, fname)) #os.path.join() joins the folder name with the file name
+             if (ingredient in recipe.lower()):
+                shutil.copyfile(fname, os.path.join(s, fname))
